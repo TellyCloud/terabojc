f5214282407370bdc89ee8a637f7796dcb7ef792
TellyCloud <156745846+TellyCloud@users.noreply.github.com>
2024-06-09 10:57:34 +0530

    Update api.js

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 async function getDetails(id) {
     try {
         const response = await axios.get(
-            `https://bot-nine-rho.vercel.app/api?data=${id}`
+            `https://teraboxvideodownloader.nepcoderdevs.workers.dev/api?data=${id}`
         );
         return response.data;
     } catch (error) {
